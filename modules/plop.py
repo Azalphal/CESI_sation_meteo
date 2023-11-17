331aba687607f7dfc3325aca66384eade18181e8
Bouyou34 <bouyou-mael@live.fr>
2023-11-17 14:25:42 +0100

    add the function file

---
+++ modules/plop.py
@@ -0,0 +1,17 @@
+def insert(type, value):
+  
+
+
+# def post():
+#     '''
+#     '''
+    
+    # hire_start = datetime.date(1999, 1, 1)
+    # hire_end = datetime.date(1999, 12, 31)
+
+    # cursor.execute(query, (hire_start, hire_end))
+    
+    # f' 
+    # INSERT INTO historique (type, date, value)
+    # VALUES (?, NOW(), ?);', [type, value])
+    # INSERT INTO table (temperature, humidite) VALUES ({temperature}, {humidite});"
