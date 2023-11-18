6229cd832d995a6f66655d0e5295714e622a6cf8
pers-ephone <148533426+pers-ephone@users.noreply.github.com>
2023-11-19 00:03:12 +0100

    make things work

--- main.py
+++ main.py
@@ -1,41 +1,50 @@
 from flask import Flask, request
 
 from modules import plop
+from env import DB_TABLE
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST', 'GET', 'PUT', 'DELETE'])
 def query():
+    """ handle sql queries """
 
     match request.method:
         case 'POST':
+            """ request body : {"temperature":, "humidite": } """
 
-            temperature = request.form.get('temperature')
-            humidite = request.form.get('humidite')
+            temperature = request.json.get('temperature')
+            humidite = request.json.get('humidite')
 
             plop.create(DB_TABLE, temperature, humidite)
+            return "1"
 
         case 'GET':
+            """ request body : {"item_id":, "key": } """
 
-            item_id = request.form.get('item_id')
-            key = request.form.get('key')
+            item_id = request.json.get("item_id")
+            key = request.json.get('key')
 
             return plop.read(DB_TABLE, item_id, key)
 
         case 'PUT':
+            """ request body : {"item_id":, "key":, "value": } """
 
-            item_id = request.form.get('item_id')
-            key = request.form.get('key')
-            value = request.form.get('value')
+            item_id = request.json.get('item_id')
+            key = request.json.get('key')
+            value = request.json.get('value')
 
             plop.update(DB_TABLE, item_id, key, value)
+            return "1"
 
         case 'DELETE':
+            """ request body : {"item_id": } """
 
-            item_id = request.form.get('item_id')
+            item_id = request.json.get('item_id')
 
             plop.delete(DB_TABLE, item_id)
+            return "1"
 
 
 if __name__ == "__main__":
--- modules/plop.py
+++ modules/plop.py
@@ -7,37 +7,60 @@ db = mysql.connector.connect(host=DB_HOST,
                              password=DB_PASSWORD,
                              database=DB_DATABASE)
 
-# Creation d'un objet curseur permettant d'executer des requetes SQL
-cursor = db.cursor()
-
 
 def create(table, temperature, humidite):
+
+    cursor = db.cursor()
 
     query = f"""
     CREATE TABLE IF NOT EXISTS {table} (
         id INT AUTO_INCREMENT PRIMARY KEY,
         temperature FLOAT,
-        humidite FLOAT
-    );
-    INSERT INTO {table} (temperature, humidite) VALUES ({temperature}, {humidite});
+        humidite FLOAT);
     """
     cursor.execute(query)
+    db.commit()
+
+    query = f"INSERT INTO `{table}` (`temperature`, `humidite`) VALUES ({temperature}, {humidite});"
+
+    cursor.execute(query)
+    db.commit()
+
+    cursor.close()
 
 
 def read(table, item_id, key):
 
+    cursor = db.cursor()
+
     query = f"SELECT {key} FROM {table} WHERE id = {item_id};"
+
     cursor.execute(query)
-    return 1 ############## return resultat
+
+    data = cursor.fetchall()
+    cursor.close()
+    return str(data) 
 
 
 def update(table, item_id, key, value):
 
+    cursor = db.cursor()
+
     query = f"UPDATE {table} SET {key} = {value} WHERE id = {item_id};"
+
     cursor.execute(query)
+    db.commit()
+
+    cursor.close()
 
 
 def delete(table, item_id):
 
+    cursor = db.cursor()
+
     query = f"DELETE FROM {table} WHERE id = {item_id};"
+
     cursor.execute(query)
+    db.commit()
+
+    cursor.close()
