0b6447fa795c88c3ee871ff5403af15de9508d89
Bouyou34 <bouyou-mael@live.fr>
2023-11-20 11:35:17 +0100

    add error handling and make returns json

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 from modules import plop
 from env import DB_TABLE
+
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -10,41 +12,46 @@ app = Flask(__name__)
 def query():
     """ handle sql queries """
 
-    match request.method:
-        case 'POST':
-            """ request body : {"temperature":, "humidite": } """
+    try:
+        match request.method:
+            case 'POST':
+                """ request body : {"temperature":, "humidite": } """
 
-            temperature = request.json.get('temperature')
-            humidite = request.json.get('humidite')
+                temperature = request.json.get('temperature')
+                humidite = request.json.get('humidite')
 
-            plop.create(DB_TABLE, temperature, humidite)
-            return "1"
+                plop.create(DB_TABLE, temperature, humidite)
+                return jsonify({"result": "1"}), 201
 
-        case 'GET':
-            """ request body : {"item_id":, "key": } """
+            case 'GET':
+                """ request body : {"item_id":, "key": } """
 
-            item_id = request.json.get("item_id")
-            key = request.json.get('key')
+                item_id = request.json.get("item_id")
+                key = request.json.get('key')
 
-            return plop.read(DB_TABLE, item_id, key)
+                result = plop.read(DB_TABLE, item_id, key)
+                return jsonify({"result": result}), 200
 
-        case 'PUT':
-            """ request body : {"item_id":, "key":, "value": } """
+            case 'PUT':
+                """ request body : {"item_id":, "key":, "value": } """
 
-            item_id = request.json.get('item_id')
-            key = request.json.get('key')
-            value = request.json.get('value')
+                item_id = request.json.get('item_id')
+                key = request.json.get('key')
+                value = request.json.get('value')
 
-            plop.update(DB_TABLE, item_id, key, value)
-            return "1"
+                plop.update(DB_TABLE, item_id, key, value)
+                return jsonify({"result": "1"}), 200
 
-        case 'DELETE':
-            """ request body : {"item_id": } """
+            case 'DELETE':
+                """ request body : {"item_id": } """
 
-            item_id = request.json.get('item_id')
+                item_id = request.json.get('item_id')
 
-            plop.delete(DB_TABLE, item_id)
-            return "1"
+                plop.delete(DB_TABLE, item_id)
+                return jsonify({"result": "1"}), 201
+            
+    except mysql.connector.Error as err:
+        return jsonify({"error": str(err)}), 500
 
 
 if __name__ == "__main__":
