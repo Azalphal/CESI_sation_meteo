fee68f17fba19f359baa027857f1e419c915258c
Bouyou34 <bouyou-mael@live.fr>
2023-12-11 15:50:06 +0100

    rebase into main

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const session = require('express-session');
+const crypto = require('crypto');
+const path = require('path');
+
+const apiRouter = require('./routes/api');
+const authRouter = require('./routes/auth');
+const ensureAuthenticated = require('./middleware/auth');
+const { pool, initDatabase } = require('./models/db.js')
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const secretKey = crypto.randomBytes(64).toString('hex');
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(session({ secret: secretKey, resave: true, saveUninitialized: true }));
+
+app.use('/auth', authRouter);
+app.use('/api', ensureAuthenticated, apiRouter);
+
+initDatabase();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const { registerUser, loginUser } = require('../models/db');
+
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    await registerUser(username, password);
+
+    res.status(201).send('User registered successfully.');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error.');
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    await loginUser(username, password);
+
+    res.status(201).send('User logged in successfully.');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error.');
+  }
+});
+
+router.get('/logout', (req, res) => {
+  try {
+    req.logout();
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error.');
+  }
+});
+
+module.exports = router;
