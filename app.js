893c3e13fc0d9d729c695af045d6187a31e11183
khalil <khalil.khechine@componize.com>
2023-12-15 16:01:15 +0100

    fix database port

--- app.js
+++ app.js
@@ -39,8 +39,4 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
 module.exports = app;
