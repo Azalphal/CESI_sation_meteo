85aab18bd5ca461fc904563d050194d00a6804fc
Azalphal <mael.nouvel@componize.com>
2023-12-18 16:22:40 +0100

    fix swagger

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
 const path = require("path");
 const app = express();
 
@@ -9,6 +10,7 @@ const probesRouter = require("./routes/probes.routes");
 const ensureAuthenticated = require("./middleware/auth");
 const {Sequelize} = require("sequelize");
 const config = require("./config/config");
+const swaggerDocument = require("./config/swagger-config");
 
 app.use(express.urlencoded({extended: true}));
 
@@ -34,9 +36,11 @@ sequelize
 app.use("/api/data", dataRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/probes", probesRouter);
+app.use("/api/docs", swaggerUi.serve);
+app.get("/api/docs", swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/views', 'login.html'));
+    res.sendFile(path.join(__dirname, "public/views", "login.html"));
 });
 
 module.exports = app;
