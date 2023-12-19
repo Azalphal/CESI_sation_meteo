b328c3b60ffb41eda9166ee0ed52439d8179c1a2
pers-ephone <148533426+pers-ephone@users.noreply.github.com>
2023-12-19 14:50:10 +0100

    add data/last

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -17,6 +17,19 @@ exports.findAll = (req, res) => {
     Data.findAll().then((result) => res.status(200).json(result));
 };
 
+exports.findLast = (req, res) => {
+    Data.findOne({
+        order: [['id', 'DESC']], // sort by id in descending order
+    })
+        .then((result) => {
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({ message: 'No data found' });
+            }
+        });
+};
+
 exports.findOne = (req, res) => {
     Data.findOne(req.body.id).then((result) => res.status(200).json(result));
 };
